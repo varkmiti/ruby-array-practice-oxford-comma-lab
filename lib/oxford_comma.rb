b98048aec4b5003bb20d3b54e946ed0c9a636484
Mark Viti <+>
2022-05-23 00:26:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1 
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    elsif array.length >= 3
+    last = array.pop
+    array << "and #{last}"
+    array.join(", ")
+    end 
 end
